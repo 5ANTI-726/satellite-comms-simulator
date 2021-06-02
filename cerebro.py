39b2c77c6a6a9afded2c996f25b706862c349fce
5ANTI-726 <a00829556@itesm.mx>
2021-06-01 20:06:39 -0400

    Most modern version. The setup for the algorithm is done.

--- cerebro.py
+++ cerebro.py
@@ -2,22 +2,40 @@
 import math
 from playsound import playsound
 
+#Cuando quieras agregar más cuerpos debes 1) hacer más archivos y
+#agregarlos al método escribir_todo() en la línea 89,
+#2) declarar el/los objeto(s) en la línea 172, 3) agregar su referencia
+#a la lista all_bodies[] en la línea 178, 4) agregarlo a todas las instancias
+#del método escribir_todo en main(), 5) agregarlo a los cálculos de
+#primeros cambios de posición y velocidad en el bloque 211-226
+#(debe ser un algoritmo recursivo basado en all_bodies), 6)
+
 #¿Qué queremos guardar? Queremos velocidades, distancia del centro,
-#energía cinética/potencial, coordenadas y momento total. Son 12 archivos.
-co1 = open("coordinates1.txt", "w+")
-co2 = open("coordinates2.txt", "w+")
+#energía cinética/potencial/total, coordenadas, momento lineal total
+#(+componentes) y momento angular después (+componentes).  Son 18 archivos.
+#Coordenadas
+co1 = open("coordenadas1.txt", "w+")
+co2 = open("coordernadas2.txt", "w+")
+#Velocidades
 vx1 = open("vx1.txt", "w+")
 vy1 = open("vy1.txt", "w+")
 vz1 = open("vz1.txt", "w+")
 vx2 = open("vx2.txt", "w+")
 vy2 = open("vy2.txt", "w+")
 vz2 = open("vz2.txt", "w+")
-radio1 = open("r1.txt", "w+")
-radio2 = open("r2.txt", "w+")
+#Energias totales
 T = open("cinetica.txt", "w+")
 U = open("potencial.txt", "w+")
+#Momentos lineales
 P = open("momento.txt", "w+")
-H = open("hamilton.txt", "w+")
+Px = open("momento.txt", "w+")
+Py = open("momento.txt", "w+")
+Pz = open("momento.txt", "w+")
+#Momentos angulares
+PA = open("momento_angular.txt", "w+")
+PAx = open("momento_angular.txt", "w+")
+PAy = open("momento_angular.txt", "w+")
+PAz = open("momento_angular.txt", "w+")
 
 #Get initial velocity and orientation.
 #Concider giving heading in terms of an angle or a different point in space.
@@ -40,29 +58,77 @@ def crash(all_bodies):
             if cum_r <= all_bodies[i].distancia(all_bodies[k]):
                 status = True
 
-    return status
+    return(status)
 
+#Energía cinética total.
+def cinetica(all_bodies):
+    return(T)
+
+#Energía potencial total.
+def potencial(all_bodies):
+    return(U)
+
+#Momento lineal total.
+def momento_lineal(all_bodies):
+    return(P)
+#Momento lineal en componente i.
+def momento_lineal_comp(all_bodies,i):
+    return(Pi)
+
+#Momento angular total.
+def momento_angular(all_bodies):
+    return(PA)
+#Momento angular en componente i.
+def momento_angular_comp(all_bodies,i):
+    return(PAi)
+
+#Aceptan cualquier tipo de datos y los convierten a tipo string.
 def escribir2(archivo,c,d):
     archivo.write('('+str(c)+','+str(d)+')')
     archivo.write("\n")
 def escribir3(archivo,e,f,g):
     archivo.write('('+str(e)+','+str(f)+','+str(g)')')
     archivo.write("\n")
-
+#Compilación de todos los datos posibles en un mismo tiempo
+#gracias a que el método es velocity verlett.
+def escribir_todo(A,B,count,delta,all_bodies):
+    #Coordenadas
+    escribir3(co1,A.pos[0],A.pos[1],A.pos[2],count+delta)
+    escribir3(co2,B.pos[0],B.pos[1],B.pos[2],count+delta)
+    #Velocidades
+    escribir2(vx1,count+delta,A.vel[0])
+    escribir2(vy1,count+delta,A.vel[1])
+    escribir2(vz1,count+delta,A.vel[2])
+    escribir2(vx2,count+delta,B.vel[0])
+    escribir2(vy2,count+delta,B.vel[1])
+    escribir2(vz2,count+delta,B.vel[2])
+    #Energía potencial
+    escribir2(U,count+delta,potencial(all_bodies))
+    #Energía cinética
+    escribir2(T,count+delta,cinetica(all_bodies))
+    #Momentos lineales
+    escribir2(P,count+delta,momento_lineal(all_bodies))
+    #escribir2(Px,count+delta,momento_lineal_comp(all_bodies,0))
+    #escribir2(Py,count+delta,momento_lineal_comp(all_bodies,1))
+    #escribir2(Pz,count+delta,momento_lineal_comp(all_bodies,2))
+    #Momentos angulares
+    escribir2(PA,count+delta,momento_angular(all_bodies))
+    #escribir2(PAx,count+delta,momento_angular_comp(all_bodies,0))
+    #escribir2(PAy,count+delta,momento_angular_comp(all_bodies,1))
+    #escribir2(PAz,count+delta,momento_angular_comp(all_bodies,2))
 
 ###########################cuerpos###########################
 class Cuerpo:
     c_grav = 6.67408*math.pow(10,-11)
-    def __init__(self, nombre, masa, pos, vel):
-        self.nombre = nombre #1
-        self.masa = masa     #2
-        self.pos[0] = x_pos   #3
-        self.pos[1] = y_pos   #4
-        self.pos[2] = z_pos   #5
-        self.vel[0] = x_vel   #6
-        self.vel[1] = y_vel   #7
-        self.vel[2] = z_vel   #8
+    def __init__(self, nombre, radio, masa, position, velocity):
+        self.nombre = nombre  #1
+        self.masa = masa      #2
+        self.radio = radio    #3
+        self.pos = position   #4
+        self.vel = velocity   #5
     def asignacion(self):
+        #Asignación del radio del cuerpo
+        self.radio = input("Radio del cuerpo: ")
         #Asignación de valores del cuerpo
         self.x_pos = input("Posición en x: ")
         self.y_pos = input("Posición en y: ")
@@ -103,17 +169,16 @@ class Cuerpo:
 
         return(d)
 
-
-
-A = Cuerpo('Tierra',5.9736*math.pow(10,24),[0,0,0],
+A = Cuerpo('Tierra',6371*math.pow(10,3),5.9736*math.pow(10,24),[0,0,0],
 [0,0,0])
-B = Cuerpo('Luna',7.34767*math.pow(10,21),[406700000,0,100000],
+B = Cuerpo('Luna',1737.1*math.pow(10,3),7.34767*math.pow(10,21),[406700000,0,100000],
 [970,0,-20])
 #Activar para asignar parámetros. Si no, dejar valores existentes.
 #A.asignacion()
 #B.asignacion()
 
 #Estructura de datos por si a caso.
+#De echo la usaremos para revisar detectar colisiones entre cualquier cuerpo.
 all_bodies = [A,B]
 
 #########################input and resolve parameters###########################
@@ -123,170 +188,105 @@ print('****************************************************************')
 #Get the number of points wanted which will be used in the modulo conditional
 #so as to only record data in the .txt files a limited number of times
 #for even the largest trajectories.
-points = float(input('Número de puntos por guardar: '))
-
-ok = False
-
-#Write down the initial position, gravity and gravity components.
-escribir3(co1,A.pos[0],A.pos[1],A.pos[2])
-escribir3(co2,B.pos[0],B.pos[1],B.pos[2])
+points = float(input('Número de puntos guardados: '))
 
 #Resolution of calculations (∆t).
-resolution = float(input('Resolución de tiempo: '))
+delta = float(input('Resolución de tiempo: '))
 
-#Simulation length in seconds.
+#Simulation length in seconds. Antes se llamaba resolution.
 time = float(input('Tiempo de simulación: '))
 
 #Establishing stores for the error (crash) variable that inidicates
 #if a crash ocurred, the frequency of writing down coordinates in terms
 #of points skipped, and the control variables.
-
 error = False
-frecuencia_escribir = round(time/points/resolution)
+#Cada cuanto tiempo escribir variables.
+frecuencia = round(time/points/resolution)
 remover = 0
+#Tiempo actual.
 count = 0
 
-#Get the first velocities ready for the leapfrog method.
-all_vel =
-[[A.vel[0],A.vel[1],A.vel[2]]
-,[B.vel[0],B.vel[1],B.vel[2]]]
+#Escribimos la primeras posiciones, velocidades, etc.
+escribir_todo(A,B,0,0,all_bodies)
 
-#Establecemos velocity vertlett con medio paso de Euler para t + ∆t/2.
-A.vel[0]
+####Establecemos primer cambio de velocidad y posición.####
+F = A.fuerza(B)
+#Cambios de posición a i(t+∆t) con la F_i(t).
+for i in range(0,2):
+    A.pos[i] = A.pos[i] + A.vel[i]*delta + (math.pow(delta,2)*F[i]/A.masa)/2
+    F[i] = -F[i]
+for i in range(0,2):
+    B.pos[i] = B.pos[i] + B.vel[i]*delta + (math.pow(delta,2)*F[i]/B.masa)/2
+    F[i] = -F[i]
+#Cambios de velocidad a v_i(t+∆t) con la F_i(t) y F_i(t+∆t).
+#Esta fuerza más avanzada es calculada con las nuevas posiciones i(t+∆t).
+F2 = A.fuerza(B)
+for i in range(0,2):
+    A.vel[i] = A.vel[i] + delta*(F[i]/A.masa + F2[i]/A.masa)/2
+    F[i] = -F[i]
+    F2[i] = -F2[i]
+for i in range(0,2):
+    B.vel[i] = B.vel[i] + delta*(F[i]/B.masa + F2[i]/B.masa)/2
 
-A.vel[0]A.fuerza(B,A.distancia(B))/A.masa
+#Escribimos nuevamente todos los datos, ahora para t = 0+∆t.
+escribir_todo(A,B,0,delta,all_bodies)
 
-ar1 = 0
-ar2 = 0
-
-#get number of decimals of the resolution
+#Decimales de la ∆t. Permitirá corregir el reloj ya que
+#no siempre se mueve por la cantidad correcta (∆t).
 decimals = 0
-holdinterval = resolution
+holdinterval = delta
 while holdinterval%1 != 0:
     holdinterval = holdinterval*10
     decimals += 1
 
+#Calcular tiempos en los que se alcanza n% de la simulación.
 checkin = []
-
 for i in range(1,101):
     point = round(time*i/100)
     checkin.append(point)
 
-#Begin simulation
+#########################Begin simulation####################################
 while count < time:
-
+    #Corregir desviaciones del tiempo actual por las decimales de ∆t.
     count = round(count,decimals)
 
-    #count progress of simulation
+    #Progreso de la simulación.
     if count in checkin:
         print(str(100*count/time),"%")
 
-    ok = False
-    if remover%espacio == 0:
-        vx.write('('+str(count+0.5*resolution)+','+str(velocity[0])+')')
-        vx.write("\n")
-        vy.write('('+str(count+0.5*resolution)+','+str(velocity[1])+')')
-        vy.write("\n")
-        ok = True
-
-    one = satellite_A[1]
-    two = satellite_A[2]
-
-    #Change position according to velocity.
-    satellite_A[1] = satellite_A[1] + velocity[0]*resolution
-    satellite_A[2] = satellite_A[2] + velocity[1]*resolution
-
-    if count <= 8000:
-        one = abs(one - satellite_A[1])
-        two = abs(two - satellite_A[2])
-        three = math.sqrt(math.pow(one,2)+math.pow(two,2))/2
-
-        if count <= 3999:
-            ar1 = ar1 + math.sqrt(math.pow(satellite_A[1],2)+
-            math.pow(satellite_A[2],2))*three
-        elif count <= 8000:
-            ar2 = ar2 + math.sqrt(math.pow(satellite_A[1],2)
-            +math.pow(satellite_A[2],2))*three
-    if count == 8000:
-            print("Area 1: ")
-            print(ar1)
-            print("Area 2: ")
-            print(ar2)
-
-
-    #Check for crash and break if occurs. Occurs after position change
-    #so that the last valid point (last loop) is recorded.
-    if Inside(satellite_A[1],satellite_A[2]):
+    #Revisames el case de colisiones antes de guardar las posiciones y
+    #demás variables ya que no ocurrirían si hubiera una colisión.
+    if crash(all_bodies):
         print("Broke")
         error = True
         break
 
-    #store acceleration data as the first step to avoid recalculating
-    #it seven times.
-    accsuite = acomp(satellite_A[1],satellite_A[2],count,ok)
-
-    #Change velocity according to position/acceleration.
-    velocity[0] = velocity[0] + resolution*accsuite[1]
-    velocity[1] = velocity[1] + resolution*accsuite[2]
-
-    #Write position and acceleration data only every certain number
-    #of steps to save memory.
-    if remover%espacio == 0:
-        #For the satellite...
-        coo.write('('+str(satellite_A[1])+','+str(satellite_A[2])+')')
-        coo.write("\n")
-        xgs.write('('+str(count)+','+str(accsuite[1])+')')
-        xgs.write("\n")
-        ygs.write('('+str(count)+','+str(accsuite[2])+')')
-        ygs.write("\n")
-        gs.write('('+str(count)+','+str(accsuite[0])+')')
-        gs.write("\n")
-
+    #Escribir todos los datos.
+    if remover%frecuencia == 0:
+        escribir_todo(A,B,count,delta,all_bodies)
     #Increase counters.
     remover = remover + 1
     count = count + resolution
 
-#Done with program.
-playsound('siren.mp3')
+####Primer caso prueba:
+####A(r = 0.05,pos[1,0,0],vel[0,0,0])
+####B(r = 0.05,pos[-1,0,0,vel[0,0,0])
+####y revisar los archivos para revisar choque. Debe de ocurrir
+####con las posiciones finales [-0.05,0,0] y [0.05,0,0]######
 
 #################################text-based UI##################################
 #If there was no crash with the planet...
 if error == False:
 
     print("\n****************************************************************")
-    print('final velocity in x: ', str(velocity[0]))
-    print('final velocity in y: ', str(velocity[1]),"\n")
-    #The [3] and [4] were never changed because velocity was retained
-    #in the velocity[] list.
-    print('Initial acceleration parameters')
-    print(acomp(satellite_A[3],satellite_A[4],0,ok))
-    print("Maximum acceleration in 'Gs': " + str(satellite_A[7]))
-
-    #Print the coordinates where max g ocurred.
-    print("At...")
-    print("x: "+ str(satellite_A[5]))
-    print("y: "+ str(satellite_A[6]))
+    print("Simulation done!!!")
+    playsound('siren.mp3')
     print("****************************************************************")
-
 
 #If you crashed...
 else:
     print('\n****************************************************************')
     print("There goes the neighborhood")
-    print("Max Gs: " + str(satellite_A[7]))
+    print("Your trajectory was deffective in accuracy")
     print("Crashed after: " + str(count) + " seconds.")
-    print("Menu: vx, vy, 'initial acomp', quit")
     print("****************************************************************")
-
-    #Allow for data retention of last velocity in x and y directions
-    #and initial acceleration parameters.
-    quit = ''
-    while quit != 'quit':
-        quit = input()
-        if quit == 'vx':
-            print('x factor velocity: ', str(velocity[0]))
-        elif quit == 'vy':
-            print('y factor velocity: ', str(velocity[1]))
-        elif quit == 'initial acomp':
-            print('Initial acceleration parameters')
-            print(acomp(satellite_A[3],satellite_A[4],0,ok))
